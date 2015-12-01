91ee416800758c514e54a9ee66a44cef095bdfe1
Mihail Slavchev <mihail.slavchev@telerik.com>
2015-12-01 11:24:06 +0200

    initial commit

---
+++ Android/CalcLib/jni/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := NativeScript
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+    LOCAL_SRC_FILES := libs/arm/libNativeScript.so
+else ifeq ($(TARGET_ARCH_ABI),armeabi)
+    LOCAL_SRC_FILES := libs/arm/libNativeScript.so
+else ifeq ($(TARGET_ARCH_ABI),x86)
+    LOCAL_SRC_FILES := libs/x86/libNativeScript.so
+endif
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_CPPFLAGS += -std=c++11
+LOCAL_MODULE    := Calc
+LOCAL_SRC_FILES := Calc.cpp
+LOCAL_SHARED_LIBRARIES := NativeScript
+include $(BUILD_SHARED_LIBRARY)
---
+++ Android/CalcLib/jni/Application.mk
@@ -0,0 +1,9 @@
+APP_ABI := armeabi-v7a x86
+
+APP_PLATFORM := android-17
+NDK_TOOLCHAIN_VERSION := 4.8
+APP_OPTIM := debug
+
+#The new ndks require this or build fails
+APP_CFLAGS += -Wno-error=format-security
+APP_CFLAGS += -g
